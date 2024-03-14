53036927cd6a9638663fd991320b307c4875a555
edmond <edmondmulera55@gmail.com>
2024-03-15 02:52:39 +0300

    completed the hero section for lg devices

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -10,22 +10,22 @@ import Clutch from "../../public/assets/Clutch.svg";
 
 export function Hero() {
   return (
-    <div className="pt-4">
-      <div className="px-[20px]">
-        <h1 className="text-center text-[32px] leading-[40px] font-medium text-[#172026]">
+    <div className="pt-4 lg:pt-10">
+      <div className="px-[20px] lg:px-[280px]">
+        <h1 className="text-center text-[32px] leading-[40px] font-medium text-[#172026] lg:text-[64px] lg:leading-[72px] ">
           Start Monitoring your website like a pro
         </h1>
-        <p className="text-center pt-6 text-[#36485C]">
+        <p className="text-center pt-6 text-[#36485C] lg:text-[18px] lg:leading-7">
           Get a bird's eye view with our customizable dashboard. Stay on top of
           things! Revamp your woek process with our game-changing feature. Boost
           productivity and effeciency.
         </p>
 
-        <div className="flex w-full pt-8">
-          <button className="bg-[#4328EB] w-1/2 py-4 px-8 text-white rounded-[4px]">
+        <div className="flex w-full pt-8 justify-center gap-6">
+          <button className="bg-[#4328EB] w-1/2 py-4 px-8 text-white rounded-[4px] lg:w-fit">
             Try for free
           </button>
-          <button className="w-1/2 text-[#4328EB] flex items-center justify-center gap-x-2">
+          <button className="w-1/2 text-[#4328EB] flex items-center justify-center gap-x-2 lg:w-fit">
             View Pricing
             <span>
               <Image src={BlurArrow} alt="Lean More" />
@@ -38,16 +38,20 @@ export function Hero() {
         <Image
           src={Gradient}
           alt="Gradient"
-          className="min-h-[500px] w-full object-cover"
+          className="min-h-[500px] w-full object-cover lg:h-auto"
         />
-        <div className="absolute bottom-5 flex-col items-center">
-          <Image src={HeroImage} alt="hero image" className="-ml-4 h-[310px]" />
+        <div className="absolute bottom-5 flex w-full flex-col items-center">
+          <Image
+            src={HeroImage}
+            alt="hero image"
+            className="-ml-4 h-[310px] sm:-m-20 sm:h-[400px] lg:-mb-28 lg:h-auto xl:w-[70%]"
+          />
 
-          <div className="flex w-full flex-col items-center">
-            <p className="text-[#FFFFFF] text-center">
+          <div className="flex w-full flex-col items-center lg:container lg:flex-row lg:justify-between lg:px-20">
+            <p className="text-[#FFFFFF] text-center lg:text-[18px]">
               Trusted by these companies
             </p>
-            <div className="grid grid-cols-3 items-center justify-center justify-items-center px-[20px] align-middle">
+            <div className="grid grid-cols-3 items-center justify-center justify-items-center px-[20px] align-middle lg:grid-cols-5">
               <Image src={Google} alt="google" />
               <Image src={Slack} alt="slack" />
               <Image src={Trustpilot} alt="Trustpilot" />
