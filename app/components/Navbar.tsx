c7ee921072c7e0e08e9f493a33df365079f2e16c
edmond <edmondmulera55@gmail.com>
2024-03-15 01:27:46 +0300

    add assets

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,19 +1,24 @@
 import Image from "next/image";
-import Logo from "../../public/assests/Logo.svg";
-import User from "../../public/assests/User.svg";
-import Menu from "../../public/assests/Menu.svg";
+import Logo from "../../public/assets/Logo.svg";
+import User from "../../public/assets/User.svg";
+import Menu from "../../public/assets/Menu.svg";
+
+const navLinks = [];
 
 export function Navbar() {
   return (
-    <nav className="flex w-full items-center justify-between px-[20px] py-[16px]">
+    <nav className="flex w-full items-center justify-between px-[20px] py-[16px] lg:container lg:mx-auto lg:px-20">
       <div>
         <Image src={Logo} alt="logo" />
       </div>
       <div className="flex gap-5">
-        <div>
+        <div className="flex items-center gap-2">
           <Image src={User} alt="User profile" />
+          <span className="hidden font-medium text-[#36465C] lg:block">
+            Sign in
+          </span>
         </div>
-        <Image src={Menu} alt="menu icon" />
+        <Image src={Menu} alt="menu icon" className="lg:hidden" />
       </div>
     </nav>
   );
