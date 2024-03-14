b8123bee571900b05b95c78ed5d429406572924d
edmond <edmondmulera55@gmail.com>
2024-03-15 01:51:20 +0300

    complete navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,22 +3,45 @@ import Logo from "../../public/assets/Logo.svg";
 import User from "../../public/assets/User.svg";
 import Menu from "../../public/assets/Menu.svg";
 
-const navLinks = [];
+const navLinks = [
+  { name: "Features" },
+  { name: "Pricing" },
+  { name: "Enterprise" },
+  { name: "Careers" },
+];
 
 export function Navbar() {
   return (
     <nav className="flex w-full items-center justify-between px-[20px] py-[16px] lg:container lg:mx-auto lg:px-20">
-      <div>
-        <Image src={Logo} alt="logo" />
+      <div className="flex items-center">
+        <Image src={Logo} alt="logo" className="cursor-pointer" />
+
+        <div className="hidden lg:flex pl-[74px] gap-x-[56px]">
+          {navLinks.map((item, index) => (
+            <p
+              key={index}
+              className="text-[#36485C] font-medium cursor-pointer"
+            >
+              {item.name}
+            </p>
+          ))}
+        </div>
       </div>
       <div className="flex gap-5">
+        <p className="hidden lg:block font-medium text-[#36485C] pr-[56px] cursor-pointer">
+          Open an Account
+        </p>
         <div className="flex items-center gap-2">
-          <Image src={User} alt="User profile" />
-          <span className="hidden font-medium text-[#36465C] lg:block">
+          <Image src={User} alt="User profile" className="cursor-pointer" />
+          <span className="hidden font-medium text-[#36465C] lg:block cursor-pointer">
             Sign in
           </span>
         </div>
-        <Image src={Menu} alt="menu icon" className="lg:hidden" />
+        <Image
+          src={Menu}
+          alt="menu icon"
+          className="lg:hidden cursor-pointer"
+        />
       </div>
     </nav>
   );
