505fda73b68f5d2cbec2c5fb5f579364720422ad
brian-ambani <brainambani1@gmail.com>
2024-03-16 03:18:29 +0300

    add sales monitoring feature for lg devices

--- app/components/Features.tsx
+++ app/components/Features.tsx
@@ -7,20 +7,31 @@ import bluebutton from "../../public/assets/blue-button.svg";
 
 export function Features() {
   return (
-    <div className="flex flex-col gap-y-[56px] py-[56px]">
-      <div className="flex flex-col">
-        <div>
-          <h3 className="font-medium text-[#0085FF]">Sales Monitoring</h3>
-          <h1 className="font-medium pt-[12px] text-2xl text-[#172026]">
+    <div className="flex flex-col gap-y-[56px] py-[56px] lg:py-[120px] lg:gap-y-[80px]">
+      <div className="flex flex-col gap-x-6 sm:flex-row-reverse">
+        <Image
+          src={Feature1}
+          alt="feature image"
+          className="hidden w-1/2 sm:block"
+        />
+        <div className="sm:w-1/2 lg:py-[56px] lg:pr-[56px]">
+          <h3 className="font-medium text-[#0085FF] lg:text-[18px]">
+            Sales Monitoring
+          </h3>
+          <h1 className="font-medium pt-[12px] text-2xl text-[#172026] lg:text-[42px] lg:leading-[58px]">
             Simply your sales monitoring
           </h1>
-          <Image src={Feature1} alt="feature 1 image" className="pt-[24px]" />
-          <p className="py-[20px] text-[#36485C]">
+          <Image
+            src={Feature1}
+            alt="feature 1 image"
+            className="pt-[24px] sm:hidden"
+          />
+          <p className="py-[20px] text-[#36485C] lg:text-[18px]">
             Stay on top of things and revamp your work process with our
             game-changing feature. Get a bird's eye view with our customizable
             dashboard.
           </p>
-          <ul className="flex flex-col gap-y-3">
+          <ul className="flex flex-col gap-y-3 lg:text-[18px]">
             <li className="flex items-center gap-x-2 text-[#36485C]">
               <span>
                 <Image src={Check} alt="check mark" />
@@ -40,7 +51,7 @@ export function Features() {
               Lorem ipsum dolor sit.
             </li>
           </ul>
-          <p className="pt-[24px] flex items-center gap-x-2 font-medium text-[#0085FF]">
+          <p className="pt-[24px] flex items-center gap-x-2 font-medium text-[#0085FF] lg:text-[18px]">
             Learn more
             <span>
               <Image src={bluebutton} alt="Learn more" />
