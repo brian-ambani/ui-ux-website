8d0fe7dd63e8de290409e3d91f0474f81631a249
brian-ambani <brainambani1@gmail.com>
2024-03-16 03:18:29 +0300

    add customer support feauture both for mobile and lg devices

--- app/components/Features.tsx
+++ app/components/Features.tsx
@@ -4,23 +4,35 @@ import Feature2 from "../../public/assets/feature-2.svg";
 import Feature3 from "../../public/assets/feature-3.svg";
 import Check from "../../public/assets/check.svg";
 import bluebutton from "../../public/assets/blue-button.svg";
+import greenbutton from "../../public/assets/green-button.svg";
 
 export function Features() {
   return (
-    <div className="flex flex-col gap-y-[56px] py-[56px]">
-      <div className="flex flex-col">
-        <div>
-          <h3 className="font-medium text-[#0085FF]">Sales Monitoring</h3>
-          <h1 className="font-medium pt-[12px] text-2xl text-[#172026]">
+    <div className="flex flex-col gap-y-[56px] py-[56px] lg:py-[120px] lg:gap-y-[80px]">
+      <div className="flex flex-col gap-x-6 sm:flex-row-reverse">
+        <Image
+          src={Feature1}
+          alt="feature image"
+          className="hidden w-1/2 sm:block"
+        />
+        <div className="sm:w-1/2 lg:py-[56px] lg:pr-[56px]">
+          <h3 className="font-medium text-[#0085FF] lg:text-[18px]">
+            Sales Monitoring
+          </h3>
+          <h1 className="font-medium pt-[12px] text-2xl text-[#172026] lg:text-[42px] lg:leading-[58px]">
             Simply your sales monitoring
           </h1>
-          <Image src={Feature1} alt="feature 1 image" className="pt-[24px]" />
-          <p className="py-[20px] text-[#36485C]">
+          <Image
+            src={Feature1}
+            alt="feature 1 image"
+            className="pt-[24px] sm:hidden"
+          />
+          <p className="py-[20px] text-[#36485C] lg:text-[18px]">
             Stay on top of things and revamp your work process with our
             game-changing feature. Get a bird's eye view with our customizable
             dashboard.
           </p>
-          <ul className="flex flex-col gap-y-3">
+          <ul className="flex flex-col gap-y-3 lg:text-[18px]">
             <li className="flex items-center gap-x-2 text-[#36485C]">
               <span>
                 <Image src={Check} alt="check mark" />
@@ -40,10 +52,62 @@ export function Features() {
               Lorem ipsum dolor sit.
             </li>
           </ul>
-          <p className="pt-[24px] flex items-center gap-x-2 font-medium text-[#0085FF]">
+          <p className="pt-[24px] flex items-center gap-x-2 font-medium text-[#0085FF] lg:text-[18px]">
             Learn more
             <span>
               <Image src={bluebutton} alt="Learn more" />
+            </span>
+          </p>
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-x-6 sm:flex-row">
+        <Image
+          src={Feature2}
+          alt="feature image"
+          className="hidden w-1/2 sm:block"
+        />
+        <div className="sm:w-1/2 lg:py-[56px] lg:pl-[56px]">
+          <h3 className="font-medium text-[#00A424] lg:text-[18px]">
+            Customer Support
+          </h3>
+          <h1 className="font-medium pt-[12px] text-2xl text-[#172026] lg:text-[42px] lg:leading-[58px]">
+            Get in touch with your customers
+          </h1>
+          <Image
+            src={Feature2}
+            alt="feature 1 image"
+            className="pt-[24px] sm:hidden"
+          />
+          <p className="py-[20px] text-[#36485C] lg:text-[18px]">
+            Stay on top of things and revamp your work process with our
+            game-changing feature. Get a bird's eye view with our customizable
+            dashboard.
+          </p>
+          <ul className="flex flex-col gap-y-3 lg:text-[18px]">
+            <li className="flex items-center gap-x-2 text-[#36485C]">
+              <span>
+                <Image src={Check} alt="check mark" />
+              </span>
+              Lorem ipsum dolor sit.
+            </li>
+            <li className="flex items-center gap-x-2 text-[#36485C]">
+              <span>
+                <Image src={Check} alt="check mark" />
+              </span>
+              Lorem ipsum dolor sit.
+            </li>
+            <li className="flex items-center gap-x-2 text-[#36485C]">
+              <span>
+                <Image src={Check} alt="check mark" />
+              </span>
+              Lorem ipsum dolor sit.
+            </li>
+          </ul>
+          <p className="pt-[24px] flex items-center gap-x-2 font-medium text-[#00A424] lg:text-[18px]">
+            Learn more
+            <span>
+              <Image src={greenbutton} alt="Learn more" />
             </span>
           </p>
         </div>
